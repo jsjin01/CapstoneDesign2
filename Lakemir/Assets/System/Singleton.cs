8a4e992e220a573c1747075662791ec638a83d87
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-11-05 21:01:49 +0900

    singleton 설명 추가

--- Lakemir/Assets/System/Singleton.cs
+++ Lakemir/Assets/System/Singleton.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : Component
 {
     public static T _Instance;
+    //싱글톤에 해당하는 오브젝트는 하나만 존재하도록 설정
+
+    /// 제네릭 클래스 싱글톤 사용법
+    /// 선언 : public class UIManager : Singleton<UIManager>
+    /// 활용 : UIManager um = UIManager.Instance;
+
     public static T Instance
     {
         get
