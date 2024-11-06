a9aebe7e5488c09018ad6e28965ef5bd505e78de
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-11-06 16:45:54 +0900

    joy stick부분 및 물리 법칙 적용

--- Lakemir/Assets/Scripts/Main Game/player/PlayerMovement.cs
+++ Lakemir/Assets/Scripts/Main Game/player/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 5f;
+    public Joystick joystick;  // Joystick을 추가할 변수
 
-    // Update is called once per frame
     void Update()
     {
-        
+#if UNITY_EDITOR
+        // 개발 환경에서만 키보드 입력을 통한 이동 가능
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.position += new Vector3(moveX, moveY, 0);
+#endif
+
+        Vector3 moveDirection = new Vector3(joystick.Horizontal, joystick.Vertical, 0).normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
+
+
+
