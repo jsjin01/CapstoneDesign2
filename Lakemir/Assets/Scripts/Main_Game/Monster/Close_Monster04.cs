64f7498c907546d7357805881b7688a4deb6e6f6
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-11-27 22:18:46 +0900

    애니메이션 작업

---
+++ Lakemir/Assets/Scripts/Main_Game/Monster/Close_Monster04.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close_Monster04 : Monster
+{
+    [Header("ºÐ¿­Ã¼ prefabs")]
+    [SerializeField] GameObject miniMonster;
+
+}
