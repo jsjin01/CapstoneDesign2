fbb95f2c7b15197e8f2abad9ac466af8219415ab
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-05 20:31:22 +0900

    맵 제작 및 무기 제작

--- Lakemir/Assets/Scripts/Main_Game/Weapon/Long_Range_Weapon/WeaponID08.cs
+++ Lakemir/Assets/Scripts/Main_Game/Weapon/Long_Range_Weapon/WeaponID08.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponID07 : LongRangeWeapon
+public class WeaponID08 : LongRangeWeapon
 {
     float currentIncreaseAdd = 0.05f;
     float damageSave;               //무기 강화로만 적용된 대미지 저장
-    public WeaponID07()
+    public WeaponID08()
     {
-        weaponName = "절망의 투척별";
-        description = "손바닥 크기의 검은 별 모양을 하고 있으며, 주변의 빛을 흡수하는 능력이 있어 사용자의 손에서 던져질 때 어둠 속에서도 거의 보이지 않는다." +
-            " 특히, 소리도 거의 내지 않아 적에게 몰래 접근해 빠르게 공격할 수 있음";
-        ability = "공포 유발: 명중 시에 일정 확률로(20%) 짧은 시간(0.5초)동안 기절시킨다.\n" +
-                  "어둠의 막: 재장전 전까지 사용할 때마다 추가 피해를 줌(5% / 10% /15% /20% /25%데미지)";
+        weaponName = "빛의 파동 발사기";
+        description = "레이크미르 왕국의 고대 기술을 기반으로 제작된 고급 에너지 무기임. 이 발사기는 압축된 빛의 에너지를 집중하여 강력한 빔 형태로 발사한다." +
+            " 이 무기를 통해 빛의 파동을 조절하며, 빠른 속도로 연속적인 공격을 할 수 있음 ";
+        ability = "빛의 유도: 빛의 파동 발사기에서 발사된 빔이 자동으로 가장 가까운 적을 추적하여 명중률을 대폭 향상시키는 기능\n\n" +
+                  "빛의 파동: 적을 명중 시 주변에 데미지를 주는 오라(15%)를 생성해냄";
         w_type = WeaponEnum.WEAPON_TYPE.LONG_RANGE_WEAPON;
         damageSave = 0.35f * (1 + 0.1f * weaponGrade);
         damage = damageSave;
@@ -29,7 +29,7 @@ public class WeaponID07 : LongRangeWeapon
         Debug.Log($"[WeaponID07] 데미지 적용 :  {damageSave + (currentIncreaseAdd * (6 - currentArrow))}");
         damage = damageSave + (currentIncreaseAdd * (6 - currentArrow));
         int randomNumber = Random.Range(1, 101);
-        if (randomNumber <= 20)
+        if(randomNumber <= 20)
         {
             hitEffect = EFFECT.STUN;
         }
